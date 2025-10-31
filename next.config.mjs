73ad725321eec7b8fd45108f14821b5a5d09a9ec
Amar Kumar <163118411+amar2mail9@users.noreply.github.com>
2025-10-31 19:49:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,18 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// next.config.js
+export default {
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          { key: 'Content-Type', value: 'application/xml' },
+          { key: 'Cache-Control', value: 'no-cache' },
+        ],
+      },
+    ];
+  },
+};
